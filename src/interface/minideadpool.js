fd2a77e509e7b2d270462a3a435dfcbc58f634bd
Tommy <thomasmilox@gmail.com>
2018-02-23 19:04:48 +0000

    refacto ui

---
+++ src/interface/minideadpool.js
@@ -0,0 +1,260 @@
+import React from 'react'
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton'
+import { Fetch } from '../logic/fetch.js'
+
+export default class MiniDeadPool extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            success: false,
+            login: false,
+            signin: false,
+            messageLogin: '',
+            messageSignin: ''
+        }
+        this.handleLogin = this.handleLogin.bind(this)
+        this.handleSignin = this.handleSignin.bind(this)
+        this.handleSubmitLogin = this.handleSubmitLogin.bind(this)
+        this.handleSubmitSignin = this.handleSubmitSignin.bind(this)
+        this.handleCloseLogin = this.handleCloseLogin.bind(this)
+        this.handleCloseSignin = this.handleCloseSignin.bind(this)
+    }
+
+    handleLogin() {
+        if (this.props.connected) {
+            this.setState({ login : true })
+        }
+    }
+
+    handleCloseLogin()  {
+        if (this.props.connected) {
+            this.setState({ login: false })
+        }
+    }
+
+    handleSignin() {
+        if (this.props.connected) {
+            this.setState({ signin : true })
+        }
+    }
+
+    handleCloseSignin()  {
+        if (this.props.connected) {
+            this.setState({ signin: false })
+        }
+    }
+
+    handleSubmitLogin(event, data) {
+        if (data === undefined) {
+            return 
+        } 
+
+        if (this.props.connected) {
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+            
+            Fetch('POST', 'http://localhost:3000/api.homecomix/authorize', headers, data)
+                .then(response => {
+                    this.props.authorize(event, response)
+                    this.setState({ messageLogin: response.message, success: false })
+                 })
+                .catch(err => { console.log(err) })
+        }  
+    }
+
+    handleSubmitSignin(event, data) {
+        if (data === undefined) {
+            return
+        } 
+
+        if (this.props.connected) {
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+
+            Fetch('POST', 'http://localhost:3000/api.homecomix/register', headers, data)
+                .then(response => { 
+                    if (response.success) {
+                        this.setState({
+                            success: true,
+                            signin: false,
+                            login: true,
+                            messageLogin: response.message
+                        })
+                    } else {
+                        this.setState({ messageSignin: response.message }) 
+                    }
+                })
+                .catch(err => { console.log(err) })
+        }
+    }
+
+    render() {
+        const actionsLogin = [
+            <FlatButton
+                key="closelogin"
+                label="Cancel"
+                primary={true}
+                onClick={this.handleCloseLogin}
+            />,
+            <FlatButton
+                key="submitlogin"
+                type="submit"
+                label="Submit"
+                primary={true}
+                onClick={this.handleSubmitLogin}
+            />
+          ]
+
+        const actionsSignin = [
+            <FlatButton
+                key="closesignin"
+                label="Cancel"
+                primary={true}
+                onClick={this.handleCloseSignin}
+            />,
+            <FlatButton
+                key="submitsignin"
+                type="submit"
+                label="Submit"
+                primary={true}
+                onClick={this.handleSubmitSignin}
+            />
+          ]
+        
+          const style = {
+              color: this.state.success ? 'green' : 'red'
+          }
+
+        return(
+            <div className="body-container">
+                <Dialog
+                    title="Login"
+                    modal={false}
+                    open={this.state.login}
+                    onRequestClose={this.handleCloseLogin}
+                    autoScrollBodyContent={true}
+                    >
+                    <form action="/" method="POST" onSubmit={(e) => { 
+                                e.preventDefault()
+                                const form = new FormData(e.target)
+                                const data = {
+                                    username: form.get('username'),
+                                    password: form.get('password')
+                                }
+                                this.handleSubmitLogin(e, data)
+                            }}>
+                        <label>
+                            Username:
+                            <input type="text" name="username" required/>
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" required/>
+                        </label>
+                        <div style={{ textAlign: 'right', padding: 8, margin: '24px -24px -24px -24px' }}>
+                            {actionsLogin}
+                        </div>
+                    </form>
+                    <span style={style}>
+                        {this.state.messageLogin}
+                    </span>
+                </Dialog>
+                <Dialog
+                    title="Sign in"
+                    modal={false}
+                    open={this.state.signin}
+                    onRequestClose={this.handleCloseSignin}
+                    autoScrollBodyContent={true}
+                    >
+                    <form action="/" method="POST" onSubmit={(e) => { 
+                                e.preventDefault()
+                                const form = new FormData(e.target)
+                                const data = {
+                                    username: form.get('username'),
+                                    email: form.get('email'),
+                                    password: form.get('password')
+                                }
+                                this.handleSubmitSignin(e, data)
+                            }}>
+                        <label>
+                            Username:
+                            <input type="text" name="username" required/>
+                        </label>
+                        <label>
+                            Password:
+                            <input type="password" name="password" required/>
+                        </label>
+                        <label>
+                            Email:
+                            <input type="email" name="email" required/>
+                        </label>
+                        <div style={{ textAlign: 'right', padding: 8, margin: '24px -24px -24px -24px' }}>
+                            {actionsSignin}
+                        </div>
+                    </form>
+                    <span>
+                        {this.state.messageSignin}
+                    </span>
+                </Dialog>
+                <div className="sword-container">
+                    <div className="sword left-sword">
+                        <div className="cross-guard"></div>
+                    </div>
+                    <div className="sword right-sword">
+                        <div className="cross-guard"></div>
+                    </div>
+                </div>
+                <div className="mask-shadow">
+                    <div className="mask">
+                        <div className="mask-shine-01"></div>
+                        <div className="mask-shine-02"></div>
+                        <div className="eyes left-eye">
+                            <div className="left-eye-pupil"></div>
+                        </div>
+                        <div className="eyes right-eye">
+                            <div className="right-eye-pupil"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="body-shadow">
+                    <div className="body-top">
+                        <div className="pecs left-pec"></div>
+                        <div className="pecs right-pec"></div>
+                        <div className="upper-body"></div>
+                        <div className="belt">
+                            <div className="ammo-thing ammo-01"></div>
+                            <div className="ammo-thing ammo-02"></div>
+                            <div className="outer-belt-buckle">
+                                <div className="inner-belt-buckle">
+                                    <div className="line-belt"></div>
+                                </div>
+                            </div>
+                            <div className="ammo-thing ammo-03"></div>
+                            <div className="ammo-thing ammo-04"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="hands-container">
+                    <div className="hands-shadow ">
+                        <div className="hand left-hand" style={{cursor: this.props.connected ? 'pointer': 'unset'}} onClick={this.handleSignin}>
+                            <div className="hand-shine"></div>
+                            <div className="hand-small-shine"></div>
+                        </div>
+                    </div>
+                    <div className="hands-shadow hands-shadow-right">
+                        <div className="hand right-hand" style={{cursor: this.props.connected ? 'pointer': 'unset'}} onClick={this.handleLogin}>
+                            <div className="hand-shine"></div>
+                            <div className="hand-small-shine"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="body-ground-shadow"></div>
+            </div>
+        )
+    }
+}
--- src/logic/authentication.js
+++ src/logic/authentication.js
@@ -1,112 +1,59 @@
 import React from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
 import { Fetch } from './fetch.js'
-import LoginForm from '../interface/loginForm.js'
-import SigninForm from '../interface/signinForm.js'
-import BatmanLogo from '../interface/batmanLogo.js'
+import MiniDeadPool from '../interface/minideadpool.js'
 
 class Authentication extends React.Component {
     constructor(props, context) {
         super(props, context);
     
         this.state = {
-            title: '',
-            message: '',
-            back: false,
-            buttons: true,
-            authorize: false,
-            register: false
-        };
+            connected: false
+        }
 
-        this.register = this.register.bind(this)
         this.authorize = this.authorize.bind(this)
     }
     
     componentDidMount() {
         Fetch('GET', 'http://localhost:3000/api.homecomix')
             .then(response => {
-                this.setState({
-                    title: response.message
-                })
+                if (response.success) {
+                    this.setState({ connected: true })
+                }
             })
-            .catch(err => { console.log(err) })
-    }
-
-    showLoginForm() {
-        this.setState({
-            back: true,
-            buttons: false,
-            authorize: true,
-            register: false
-         })
-    }
-
-    showSigninForm() {
-        this.setState({
-            back: true,
-            buttons: false,
-            authorize: false,
-            register: true
-        })
-    }
-
-    back() {
-        this.setState({
-            back: false,
-            message: '',
-            buttons: true,
-            authorize: false,
-            register: false
-        })
-    }
-
-    register(event, response) {
-        this.setState({
-            buttons: false,
-            register: true,
-            authorize: false,
-            message: response.message
-        })
-        if (response.success) {
-            this.setState({
-                authorize: true,
-                register: false
-            })
-        } 
+            .catch(err => { 
+                console.log(err)
+                this.setState({ connected: false })
+             })
     }
 
     authorize(event, response) {
         if (response.success) {
             this.props.authenticate(event, response)
-        } else {
-            this.setState({
-                buttons: false,
-                register: false,
-                authorize: true,
-                message: response.message
-           })
-       }
+        }
     }
   
     render() {
-        const style = {
-            'display': this.state.buttons ? delete this.display : 'none'
-        }
-
         return  (
             <div className="auth-div">
-                <h4>{this.state.title}</h4>
-                <div className="batmanLogo">
-                    <BatmanLogo />
-                </div>
-                <div className="auth-form">
-                    <button className="auth-btn" style={style} onClick={this.showLoginForm.bind(this)}>Login</button>
-                    <button className="auth-btn" style={style} onClick={this.showSigninForm.bind(this)}>Register</button>
-                    {this.state.authorize && <LoginForm authorize={this.authorize}/>}
-                    {this.state.register && <SigninForm register={this.register} />}
-                    {this.state.message !== '' && <p>{this.state.message}</p>}
-                    {this.state.back && <RaisedButton label="Back" onClick={this.back.bind(this)}/>}
-                </div>
+                <section>
+                    <blockquote className="whisper bubble">
+                    {this.state.connected ?
+                        <div>
+                        Alright&nbsp;boyz&nbsp;gimme&nbsp;a&nbsp;hand...<br/>
+                        Right&nbsp;for&nbsp;<span style={{color: '#ccc', fontSize: ' 2vw', fontStyle: 'italic', fontWeight: 'bold' }}>login</span>&nbsp;...and&nbsp;...&nbsp;?<br/>
+                        Left&nbsp;for&nbsp;<span style={{color: '#ccc', fontSize: ' 2vw', fontStyle: 'italic', fontWeight: 'bold' }}>sign in</span>&nbsp; #?!?!©
+                        </div>
+                        :
+                        <div>
+                           Is the  &nbsp;
+                            <a href="https://github.com/TommyStarK/HomeComix-API" style={{color: '#ccc', fontSize: ' 2vw', fontStyle: 'italic', fontWeight: 'bold', textDecoration: 'none' }}>
+                                 HomeComix-API
+                            </a> running mate ?
+                        </div>
+                    }
+                    </blockquote>
+                    <MiniDeadPool connected={this.state.connected} authorize={this.authorize}/>
+                </section>
             </div>
         )
     }
